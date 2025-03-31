d9c79e3739763043d5492d2fe29845484da6c4e5
gbaumann24 <gilles24.baumann@hotmail.com>
2025-03-31 16:58:41 +0200

    delete vite svgr plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,12 @@
 import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import svgr from 'vite-plugin-svgr';
 
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss(), svgr()],
+	plugins: [react(), tailwindcss()],
 	server: {
 		host: true, // or '0.0.0.0'
 		port: 5173, // or your preferred port
