cd68cf870b13023142b3532b50f8c559d66b998f
gbaumann24 <gilles24.baumann@hotmail.com>
2025-04-04 08:41:23 +0200

    local port

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -59,6 +59,6 @@ app.get(/(.*)/, (req, res, next) => {
 	});
 });
 
-app.listen(PORT, '127.0.0.1', () => {
+app.listen(PORT, '0.0.0.0', () => {
 	console.log(`🚀 Server läuft auf http://localhost:${PORT}`);
 });
