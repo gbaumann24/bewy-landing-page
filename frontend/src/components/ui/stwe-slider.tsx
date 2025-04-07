b95efd2326e1cbe264e6a4487e1732305ff45cd5
gbaumann24 <gilles24.baumann@hotmail.com>
2025-04-07 21:48:09 +0200

    unused

--- frontend/src/components/ui/stwe-slider.tsx
+++ frontend/src/components/ui/stwe-slider.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Slider } from '@/components/ui/slider';
 import { Users } from 'lucide-react';
 
